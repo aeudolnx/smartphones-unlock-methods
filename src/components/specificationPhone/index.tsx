b3306456ed6739def14a42ed6cdf3fc8b59543ca
Aeudo Pereira <aeudolinux@gmail.com>
2024-09-10 20:24:25 -0300

    Foi feito exemplo de como pode ficar no fim do projeto

--- src/components/specificationPhone/index.tsx
+++ src/components/specificationPhone/index.tsx
@@ -6,7 +6,13 @@ export function SpecificationPhone() {
       <div className={styles.imgContainer}>
         <img src={Smartphone} alt="A022M" />
       </div>
-      <p>Especificacoes do Celular</p>
+      <ul>
+        <li>Especificações do Celular</li>
+        <li>Modelo: A022M</li>
+        <li>Processador: MediaTek</li>
+        <li>Androids: 10, 11</li>
+        <li>Binarios: 1, 2, 3, 4</li>
+      </ul>
     </main>
   );
 }
